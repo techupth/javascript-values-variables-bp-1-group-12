f778e625b6eeac2ba4bdfe1e4cd1f3983f945a30
Natakorn Santhip <1st.natakorn@gmail.com>
2023-09-17 21:59:02 +0700

    finish Assignment: Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstname = "Karter"
+let sureName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurantsName = "Chef's Table by Chef James"
+const total = 15000 
+const numberOfRestaurants = 1500 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+total = 5000
+numberOfRestaurants = 2500
