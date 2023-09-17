305b4dc64754f54b4c304fdade14232b8d563ddb
Kawin <kawind7@gmail.com>
2023-09-17 21:59:19 +0700

    sent Exercise#1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstName = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restuarantName = "Chef's Table by Chef James"
+const priceOnBill = 15000
+const numsRestuarantOpen = 1500
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+priceOnBill = 5000
+numsRestuarantOpen = 2500
